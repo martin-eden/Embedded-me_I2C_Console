f8983983009d2dc0e527d83d9e6bf8803b0d7f8d
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2026-04-13 02:23:58 +0200

    [.^] Code cosmetics. Grouping to reduce source code size.

--- src/me_I2C_Console.cpp
+++ src/me_I2C_Console.cpp
@@ -26,6 +26,12 @@ using namespace me_I2C_Console;
 */
 void me_I2C_Console::Scan()
 {
+  /*
+    We're trying to send zero bytes to given address
+
+    Fact that Send() succeeded means we have device at that address.
+  */
+
   me_I2C::TI2C_Master I2C;
   TUint_1 DeviceId;
   me_StreamsCollection::TZeroesInputStream DummyStream;
@@ -37,43 +43,12 @@ void me_I2C_Console::Scan()
   I2C.Init();
 
   for (DeviceId = StartId; DeviceId <= StopId; ++DeviceId)
-  {
-    TBool IsPresent;
-
-    IsPresent = I2C.Send(DeviceId, 0, &DummyStream);
-
-    if (IsPresent)
+    if (I2C.Send(DeviceId, 0, &DummyStream))
       Console.Print(DeviceId);
-  }
+
   Console.EndLine();
 
   I2C.Done();
-}
-
-/*
-  [Internal] Handler to call Scan
-*/
-static void Scan_Command(
-  TUint_2 Data [[gnu::unused]],
-  TUint_2 Instance [[gnu::unused]]
-)
-{
-  Scan();
-}
-
-/*
-  [Internal] Add Scan command
-*/
-static TBool AddScanCommand(
-  me_Menu::TMenu * Menu
-)
-{
-  const TAddress Unused = 0;
-
-  return
-    Menu->AddItem(
-      me_Menu::Freetown::ToItem("s", "Scan", Scan_Command, Unused)
-    );
 }
 
 /*
@@ -93,10 +68,9 @@ void me_I2C_Console::Read()
 
     We do not need memory for received data.
 
-    Implementation requires stream (which is merely pointer to Write()
+    Implementation requires stream (which is merely a pointer to Write()
     function), not pointer to RAM memory. And all our stream processing
-    is printing byte values in ASCII. We have another stream for that
-    task.
+    is printing byte values in ASCII. We have stream for that purpose.
   */
 
   me_I2C::TI2C_Master I2C;
@@ -118,32 +92,6 @@ void me_I2C_Console::Read()
 }
 
 /*
-  [Internal] Handler to call Read
-*/
-static void Read_Command(
-  TUint_2 Data [[gnu::unused]],
-  TUint_2 Instance [[gnu::unused]]
-)
-{
-  Read();
-}
-
-/*
-  [Internal] Add Read command
-*/
-static TBool AddReadCommand(
-  me_Menu::TMenu * Menu
-)
-{
-  const TAddress Unused = 0;
-
-  return
-    Menu->AddItem(
-      me_Menu::Freetown::ToItem("r", "Read", Read_Command, Unused)
-    );
-}
-
-/*
   Write data to device
 
   Input: DeviceAddress NumBytes Bytes+
@@ -151,13 +99,10 @@ static TBool AddReadCommand(
 void me_I2C_Console::Write()
 {
   /*
-    Same as in Read() we're setting write address to zero first.
-    That read/write address stuff is common but not supported by
-    [me_I2C].
+    Same as in Read() we're setting write address to zero first
   */
 
   me_I2C::TI2C_Master I2C;
-
   TUint_1 DeviceId;
   TUint_2 NumBytes;
 
@@ -174,6 +119,28 @@ void me_I2C_Console::Write()
 }
 
 /*
+  [Internal] Handler to call Scan
+*/
+static void Scan_Command(
+  TUint_2 Data [[gnu::unused]],
+  TUint_2 Instance [[gnu::unused]]
+)
+{
+  Scan();
+}
+
+/*
+  [Internal] Handler to call Read
+*/
+static void Read_Command(
+  TUint_2 Data [[gnu::unused]],
+  TUint_2 Instance [[gnu::unused]]
+)
+{
+  Read();
+}
+
+/*
   [Internal] Handler to call Write
 */
 static void Write_Command(
@@ -185,30 +152,23 @@ static void Write_Command(
 }
 
 /*
-  [Internal] Add Write command
-*/
-static TBool AddWriteCommand(
-  me_Menu::TMenu * Menu
-)
-{
-  const TAddress Unused = 0;
-
-  return
-    Menu->AddItem(
-      me_Menu::Freetown::ToItem("w", "Write", Write_Command, Unused)
-    );
-}
-
-/*
   Add our commands to menu
 */
 void me_I2C_Console::AddCommands(
   me_Menu::TMenu * Menu
 )
 {
-  AddScanCommand(Menu);
-  AddReadCommand(Menu);
-  AddWriteCommand(Menu);
+  const TAddress Unused = 0;
+
+  Menu->AddItem(
+    me_Menu::Freetown::ToItem("s", "Scan", Scan_Command, Unused)
+  );
+  Menu->AddItem(
+    me_Menu::Freetown::ToItem("r", "Read", Read_Command, Unused)
+  );
+  Menu->AddItem(
+    me_Menu::Freetown::ToItem("w", "Write", Write_Command, Unused)
+  );
 }
 
 /*
